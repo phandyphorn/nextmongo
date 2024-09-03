960fbabc350ee7d3288030b482e38b508bb1073c
phandyphorn <phornphandy20@gmail.com>
2024-09-03 14:01:17 +0700

    develop crud

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -29,12 +29,6 @@ async function dbConnect() {
           .catch((err) => console.log("MongoDB connection error: ", err));
         return mongoose;
       });
-
-    mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
-    });
   }
   try {
     cached.conn = await cached.promise;
