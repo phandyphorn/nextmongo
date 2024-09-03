266711082d32fa3e8af608eba44fa1cc24426324
phandyphorn <phornphandy20@gmail.com>
2024-09-03 14:01:17 +0700

    develop crud

--- src/app/api/customers/route.ts
+++ src/app/api/customers/route.ts
@@ -1,10 +1,10 @@
 import Customer from "@/models/Customer";
 import dbConnect from "../../lib/db";
 import { NextResponse } from "next/server";
+import { headers } from "next/headers";
 
 // Handle GET requests
-export async function GET() {
-  let client;
+export async function GET(req: Request) {
   try {
     await dbConnect();
 
@@ -12,7 +12,17 @@ export async function GET() {
     const customers = await Customer.find({})
       .sort({ metacritic: -1 })
       .limit(10);
-    return NextResponse.json(customers);
+    const res = NextResponse.json(customers);
+
+    // Set CORS headers
+    // res.headers.set(
+    //   "Access-Control-Allow-Origin",
+    //   "https://vercel.com/phandyphorns-projects-9bdc8697/schs/71Toa1tiGjn9bop3aVxRhPDkaLyG"
+    // ); // Allow from any origin
+    // res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allowed HTTP methods
+    // res.headers.set("Access-Control-Allow-Headers", "Content-Type"); // Allowed headers
+
+    return res;
   } catch (error) {
     console.error("Failed to fetch customers:", error);
     return NextResponse.json(
